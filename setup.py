d51aaddb889ee99b51f171ed030bb5d364d20ce1
joaoxsouls <joaoxsouls@gmail.com>
2013-09-08 00:14:53 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@ from setuptools import setup
 setup(name='sentry_now',
       version='0.1',
       description='Sentry instance based on Python-2.7 Community Cartridge',
-      author=u'José Moreira',
-      author_email='zemanel@zemanel.eu',
+      author=u'Joao Oliveira',
+      AuthorO_email='joaoxsouls@gmail.com',
       url='https://github.com/getsentry/sentry/',
       install_requires=[
-          'sentry==5.4.5',
-          'psycopg2',
-          # testing
-          'selenium==2.32.0',
-          'flake8==2.0'
+          'sentry==6.2.1',
+          'psycopg2==2.5.1',
       ],
-      )
+)
