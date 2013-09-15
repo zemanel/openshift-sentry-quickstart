fc3cf44a10106541274abbe25ae4ceff5d5d7288
joaoxsouls <joaoxsouls@gmail.com>
2013-09-15 01:29:51 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(name='sentry_now',
-      version='0.1',
-      description='Sentry instance based on Python-2.7 Community Cartridge',
-      author=u'Joao Oliveira',
-      AuthorO_email='joaoxsouls@gmail.com',
-      url='https://github.com/getsentry/sentry/',
-      install_requires=[
-          'sentry==6.2.1',
-          'psycopg2==2.5.1',
-      ],
+setup(
+    name='sentry_now',
+    version='0.1',
+    description='Sentry instance based on Python-2.7 Community Cartridge',
+    author=u'José Moreira, Joao Oliveira',
+    AuthorO_email='joaoxsouls@gmail.com',
+    url='https://github.com/getsentry/sentry/',
+    install_requires=[
+        'sentry==6.2.1',
+        'psycopg2==2.5.1',
+        'flake8'
+    ],
 )
